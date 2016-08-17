b3a1984567033c6b4d4e7287462edb8dba91cad8
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-08-17 17:28:05 +0100

    Improved test file

--- spec/test.hs
+++ spec/test.hs
@@ -1,1 +1,7 @@
-main = print "HI"
+primes :: [Integer]
+primes = filterPrime [2..]
+  where filterPrime (p:xs) =
+          p : filterPrime [x | x <- xs, x `mod` p /= 0]
+
+main :: IO ()
+main = print$ show $ take 10 primes
