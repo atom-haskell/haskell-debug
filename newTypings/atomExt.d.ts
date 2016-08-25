3ec8ed0419e4d220886d448983cd470e482d08b6
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-08-25 17:46:00 +0100

    Added CompositeDisposable definition

--- newTypings/atomExt.d.ts
+++ newTypings/atomExt.d.ts
@@ -6,4 +6,10 @@ import Emissary = require("emissary");
 declare module "atom"{
     class Emitter extends Emissary.Emitter{
     }
+    class CompositeDisposable{
+        dispose(): void;
+        add(...disposables: AtomCore.Disposable[]): void;
+        remove(disposable: AtomCore.Disposable[]): void;
+        clear(): void;
+    }
 }
