babecab4d94f1330f4c883764ae8163c26c51469
Nikolay Yakimov <root@livid.pp.ru>
2017-07-17 07:36:33 +0300

    Tooltips: get closest identifier from grammar scope on empty range

--- src/lib/TooltipOverride.ts
+++ src/lib/TooltipOverride.ts
@@ -1,6 +1,12 @@
 class TooltipOverride {
     async tooltipHandler (editor: AtomTypes.TextEditor, crange: AtomTypes.Range, type: UPI.TEventRangeType)
       : Promise<UPI.ITooltipData | undefined> {
+      if (crange.isEmpty()) {
+        crange = editor.bufferRangeForScopeAtPosition('identifier.haskell', crange.start)
+      }
+      if (crange.isEmpty()) {
+        return
+      }
       const debugValue = await this.resolveExpression(editor.getTextInBufferRange(crange))
       if (debugValue !== undefined) {
           return {
